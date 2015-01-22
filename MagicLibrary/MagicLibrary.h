f65c7df6dd4c4b5712987c4669adec8515190b9b
Swapnil Agarwal <swapnil@timescity.com>
2015-01-22 15:31:23 +0530

    Initial Commit

---
+++ MagicLibrary/MagicLibrary.h
@@ -0,0 +1,13 @@
+//
+//  MagicLibrary.h
+//  MagicLibrary
+//
+//  Created by Swapnil Agarwal on 22/01/15.
+//  Copyright (c) 2015 Magic Corporations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MagicLibrary : NSObject
+
+@end
