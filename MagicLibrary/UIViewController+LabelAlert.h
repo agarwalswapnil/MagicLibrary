41f27f8c09766c7711f5af08a34dbe67efeb785d
Swapnil Agarwal <swapnil@timescity.com>
2015-01-22 17:08:45 +0530

    adding category for label alert

---
+++ MagicLibrary/UIViewController+LabelAlert.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+LabelAlert.h
+//  MagicLibrary
+//
+//  Created by Swapnil Agarwal on 22/01/15.
+//  Copyright (c) 2015 Magic Corporations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (LabelAlert)
+
+@end
